6e037d96784ecd10b86ee08de53c9dc61ba54e97
Chuiwen Ma <machuiwen@gmail.com>
2015-11-09 23:02:51 -0800

    Basic setup

---
+++ Breakout/BreakoutView.swift
@@ -0,0 +1,15 @@
+//
+//  BreakoutView.swift
+//  Breakout
+//
+//  Created by Chuiwen Ma on 11/9/15.
+//  Copyright © 2015 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class BreakoutView: UIView {
+
+    
+
+}
---
+++ Breakout/BreakoutViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BreakoutViewController.swift
+//  Breakout
+//
+//  Created by Chuiwen Ma on 11/9/15.
+//  Copyright © 2015 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class BreakoutViewController: UIViewController {
+
+    @IBOutlet weak var gameView: BreakoutView!
+
+}
